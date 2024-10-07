baee4fed44bfca92e03cf55e517a882241958641
Lola Dambrine <lola.dambrine@etu.u-bordeaux.fr>
2024-10-07 16:01:03 +0200

    rename de net_graph en chord_diag et debut de l'html pr page 3

--- data/data_manager.py
+++ data/data_manager.py
@@ -158,28 +158,3 @@ class DataManager:
     # Construire la matrice des collaborations entre genres
     genre_matrix = pd.crosstab(df_collaborations['Genre1'], df_collaborations['Genre2'])
     return genre_matrix
-
-# Fonction pour créer le Chord Diagram
-    def plot_chord_diagram(genre_matrix):
-        genres = genre_matrix.index.tolist()
-        matrix_values = genre_matrix.values
-
-        # Créer le diagramme de chord
-        fig = go.Figure(data=[go.Chord(
-            labels=genres,
-            matrix=matrix_values,
-            colorscale='Blues'
-        )])
-
-        fig.update_layout(title_text="Chord Diagram des collaborations entre genres musicaux",
-                      font=dict(size=14))
-        fig.show()
-
-    # Appel des fonctions
-    data_manager = DataManager()
-
-    # Créer la matrice de collaboration entre genres
-    genre_matrix = create_genre_collaboration_matrix(data_manager.tracks_collection, data_manager.artists_collection)
-
-    # Générer et afficher le diagramme de chord
-    plot_chord_diagram(genre_matrix)
