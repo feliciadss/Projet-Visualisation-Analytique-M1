50c2de716cfc7f511c5c135ee4401eaf37b2b0ab
Felicia Dossou <felicia.dossou@etu.u-bordeaux.fr>
2024-11-17 09:48:03 +0100

    ajout dictionnaire pays -> drapeau

--- static/enumerations.py
+++ static/enumerations.py
@@ -45,3 +45,53 @@ genre_links = {
             "musique": ("de la", "https://fr.wikipedia.org/wiki/Histoire_de_la_musique")
         }
 
+flags = {
+    "AL": "🇦🇱",  # Albania
+    "AD": "🇦🇩",  # Andorra
+    "AT": "🇦🇹",  # Austria
+    "BY": "🇧🇾",  # Belarus
+    "BE": "🇧🇪",  # Belgium
+    "BA": "🇧🇦",  # Bosnia and Herzegovina
+    "BG": "🇧🇬",  # Bulgaria
+    "HR": "🇭🇷",  # Croatia
+    "CY": "🇨🇾",  # Cyprus
+    "CZ": "🇨🇿",  # Czech Republic
+    "DK": "🇩🇰",  # Denmark
+    "EE": "🇪🇪",  # Estonia
+    "FI": "🇫🇮",  # Finland
+    "FR": "🇫🇷",  # France
+    "DE": "🇩🇪",  # Germany
+    "GR": "🇬🇷",  # Greece
+    "HU": "🇭🇺",  # Hungary
+    "IS": "🇮🇸",  # Iceland
+    "IE": "🇮🇪",  # Ireland
+    "IT": "🇮🇹",  # Italy
+    "LV": "🇱🇻",  # Latvia
+    "LI": "🇱🇮",  # Liechtenstein
+    "LT": "🇱🇹",  # Lithuania
+    "LU": "🇱🇺",  # Luxembourg
+    "MT": "🇲🇹",  # Malta
+    "MD": "🇲🇩",  # Moldova
+    "MC": "🇲🇨",  # Monaco
+    "ME": "🇲🇪",  # Montenegro
+    "NL": "🇳🇱",  # Netherlands
+    "MK": "🇲🇰",  # North Macedonia
+    "NO": "🇳🇴",  # Norway
+    "PL": "🇵🇱",  # Poland
+    "PT": "🇵🇹",  # Portugal
+    "RO": "🇷🇴",  # Romania
+    "RU": "🇷🇺",  # Russia
+    "SM": "🇸🇲",  # San Marino
+    "RS": "🇷🇸",  # Serbia
+    "SK": "🇸🇰",  # Slovakia
+    "SI": "🇸🇮",  # Slovenia
+    "ES": "🇪🇸",  # Spain
+    "SE": "🇸🇪",  # Sweden
+    "CH": "🇨🇭",  # Switzerland
+    "TR": "🇹🇷",  # Turkey
+    "UA": "🇺🇦",  # Ukraine
+    "GB": "🇬🇧",  # United Kingdom
+    "VA": "🇻🇦",  # Vatican City
+}
+
+
