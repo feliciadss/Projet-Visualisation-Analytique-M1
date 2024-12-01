5e265a05045c6f2199b98419c061c5afb1bf4001
Felicia Dossou <felicia.dossou@etu.u-bordeaux.fr>
2024-12-01 12:28:20 +0100

    modifs

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from dash import Dash
 
-# Initialisation de l'application avec support multipage
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
-
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8051)
